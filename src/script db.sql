2943bff1daaae0646b9cebc0290b6625bf5c67a9
Antonio Cortés <81495819+cortesantonio@users.noreply.github.com>
2022-05-24 12:26:42 -0400

    Update script db.sql

--- src/script db.sql
+++ src/script db.sql
@@ -23,24 +23,43 @@ create table Usuario(
 );
 
 
+
+create table categoria_Producto(
+	id int,
+    categoria varchar(200),
+    primary key(id)
+
+);
+
 create table Producto(
 	id int,
     nombre varchar(200),
     descripcion varchar(255),
     precio int,
     img text,
-    primary key (id)
+    id_categoria int,
+    primary key (id),
+	FOREIGN KEY (id_categoria) REFERENCES categoria_Producto(id)
+
 );
 
-
+create table cliente(
+	rut varchar(200),
+    primary key(rut)
+)
+;
 create table Boleta(
 	id int,
-	detalles text, 
 	total int,
 	fecha date,
+    iva int,
+    id_cliente varchar(200),
     vendedor_emisor varchar(50),
 	primary key (id),
-	FOREIGN KEY (vendedor_emisor) REFERENCES usuario(rut)
+	FOREIGN KEY (vendedor_emisor) REFERENCES usuario(rut),
+	FOREIGN KEY (id_cliente) REFERENCES cliente(rut)
+
+
 
 );
 
@@ -50,37 +69,44 @@ create table Factura(
 	razon_social varchar(200),
 	rut varchar(200),
 	direccion varchar(200),
-	detalles text, 
 	giro int,
 	iva int,
 	neto int,
 	fecha date,
 	vendedor_emisor varchar(50),
 	FOREIGN KEY (vendedor_emisor) REFERENCES usuario(rut),
+	FOREIGN KEY (rut) REFERENCES cliente(rut),
+	
 
 	primary key (id)
 );
 
-create table Informe(
-	fecha date,
-    id_Facturas int,
-    id_boletas int,
-    ventaTotal int,
-    ventaTotal_Facturas int,
-	ventaTotal_Boletas int,
-    primary key (fecha),
-    FOREIGN KEY (id_Facturas) REFERENCES Factura(id),
-    FOREIGN KEY (id_Boletas) REFERENCES Boleta(id)
 
+ 
+create table detalles(
+	id int,
+    id_producto int,
+    cantidad int,
+    id_documento int, 
+    precio int,
+    primary key(id),
+	FOREIGN KEY (id_producto) REFERENCES producto(id),
+	FOREIGN KEY (id_documento) REFERENCES  factura(id),
+	FOREIGN KEY (id_documento) REFERENCES  boleta(id)
+
+    
+    
 );
 
 create table jornada(
 	fecha date,
     id_informe date,
-    trabajador varchar(255),
+    id_trabajador varchar(50),
     EstadoJornada date,
     primary key (fecha),
-	FOREIGN KEY (id_informe) REFERENCES Informe(fecha),
-    FOREIGN KEY (EstadoJornada) REFERENCES Estado_Jornada(id_Jornada)
+    FOREIGN KEY (EstadoJornada) REFERENCES Estado_Jornada(id_Jornada),
+	FOREIGN KEY (id_trabajador) REFERENCES usuario(rut)
+
+    
 
 );
