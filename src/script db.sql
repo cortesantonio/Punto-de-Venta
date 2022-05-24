eea7a0d20ec4f811d016db376f288c646e70f3cb
Antonio Cortés <81495819+cortesantonio@users.noreply.github.com>
2022-05-24 00:01:31 -0400

    Update script db.sql

--- src/script db.sql
+++ src/script db.sql
@@ -1,20 +1,82 @@
-CREATE DATABASE puntodeventa;
+create database puntodeventa;
 
-USE puntodeventa;
+use puntodeventa;
 
-CREATE TABLE usuario(
-    rut VARCHAR(16),
-    password VARCHAR(255),
-    nombre VARCHAR(200),
-    rol int(1),
-    PRIMARY KEY(rut)
+create table Estado_Jornada(
+	id_Jornada date,
+	Estado varchar(50),
+    primary key (id_Jornada)
 );
 
-CREATE TABLE producto(
-    codigo int(11),
-    nombre VARCHAR(50),
-    descripcion VARCHAR(255),
-    precio int(10),
-    url_img VARCHAR(255)
-    PRIMARY KEY(codigo)
+create table Tipo_Usuario(
+	id int,
+	tipo varchar(50),
+    primary key (id)
+);
+create table Usuario(
+	rut varchar(50),
+    password varchar(200),
+    nombre varchar(200),
+    id_TipoUsuario int,
+    primary key (rut),
+    FOREIGN KEY (id_TipoUsuario) REFERENCES Tipo_Usuario(id)
+);
+
+
+create table Producto(
+	id int,
+    nombre varchar(200),
+    descripcion varchar(255),
+    precio int,
+    primary key (id)
+);
+
+
+create table Boleta(
+	id int,
+	detalles text, 
+	total int,
+	fecha date,
+    vendedor_emisor varchar(30),
+	primary key (id)
+);
+
+
+create table Factura(
+	id int,
+	razon_social varchar(200),
+	rut varchar(200),
+	direccion varchar(200),
+	detalles text, 
+	giro int,
+	iva int,
+	neto int,
+	fecha date,
+	vendedor_emisor varchar(30),
+
+	primary key (id)
+);
+
+create table Informe(
+	fecha date,
+    id_Facturas int,
+    id_boletas int,
+    ventaTotal int,
+    ventaTotal_Facturas int,
+	ventaTotal_Boletas int,
+    primary key (fecha),
+    FOREIGN KEY (id_Facturas) REFERENCES Factura(id),
+    FOREIGN KEY (id_Boletas) REFERENCES Boleta(id)
+
+);
+
+create table jornada(
+	fecha date,
+    id_informe date,
+    trabajador varchar(255),
+    EstadoJornada date,
+    primary key (fecha),
+	FOREIGN KEY (id_informe) REFERENCES Informe(fecha),
+    FOREIGN KEY (EstadoJornada) REFERENCES Estado_Jornada(id_Jornada)
+
 );
